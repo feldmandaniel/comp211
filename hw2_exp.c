bbe01cbc39536cbd146468d2df370df142e2baa3
feldman-daniel <dfeldman01@wesleyan.edu>
2021-03-04 20:41:37 -0500

    final

--- hw2_exp.c
+++ hw2_exp.c
@@ -1,34 +1,43 @@
 #include <stdio.h>
 
 unsigned long factorial(unsigned long x){
-  int res_fact = 1;
+  unsigned long res_fact = 1;
   for(int i = 1; x >= i; i++){
     res_fact = (res_fact * i);
-    printf("factorial of %i = %i\n", i, res_fact);
 	}
-	printf("final factorial = %i\n", res_fact);
 	return res_fact;
 }
 
-unsigned long power(unsigned long y, unsigned long expo){
+unsigned long calculate_power(unsigned long y, unsigned long expo){
 	unsigned long res_pow = 1;
-	for (int i = 1; expo >= i; i++){
+	for (unsigned long i = 1; expo >= i; i++){
 		res_pow = res_pow * y;
-		printf("%lu to the power of %lu = %lu\n", y, expo, res_pow);
 	}
 	return res_pow;
 }
 
-int main() {
-	unsigned long fact_var, pow_var, pow_exp;
-  printf("Get the factorial of:");
-  scanf("%lu", &fact_var);
-	factorial(fact_var);
-	printf("Get ");
-	scanf("%lu", &pow_var);
-	printf("to the power of ");
-  scanf("%lu", &pow_exp);
-	power(pow_var, pow_exp);
+unsigned long calculate_e(unsigned long n){
+	unsigned long power = 0, pow_fact;
+	double pow_pow, e = 1;
+	long double eps = 0.00000001, lastterm = 1;
+	while (lastterm > eps){
+		power = power + 1;
+		pow_fact = factorial(power);
+		pow_pow = calculate_power(n, power);
+		printf("factorial of %lu = %lu, %lu to the power of %lu = %f\n", power, pow_fact, n, power, pow_pow);
+		lastterm = pow_pow / pow_fact;
+		printf("lastterm = %Lf, at iteration = %lu\n", lastterm, power);
+		e = e + lastterm;
+			printf("e = %f\n", e);
+	}
+	printf("e to the power of %lu = %f\n", n, e);
+	return e;
+}
 
+int main() {
+	unsigned long e_var;
+  printf("Get e to the power of: ");
+  scanf("%lu", &e_var);
+	calculate_e(e_var);
   return 0;
 }
