4cb0f1c708b1607e86544ec5ac5ff67ac8fe8ee4
feldman-daniel <dfeldman01@wesleyan.edu>
2021-03-05 11:06:58 -0500

    loan final, pretty cool

--- hw2_loan.c
+++ hw2_loan.c
@@ -6,56 +6,48 @@ long double monthly_pay(long double loan_amount, long double term, long double a
   term = term * 12;
   apr_synth = pow(1 + apr/12, term);
   mon_pay = (loan_amount * apr_synth * apr) / (12 * (apr_synth - 1));
-  printf("loan amount = %Lf, term = %Lf, monthly pay = %Lf, and apr = %Lf\n",loan_amount, term, mon_pay, apr_synth);
-
   return mon_pay;
   }
 
-long double monthly_balance(long double loan_amount, long double term, long double mon_pay){
-  long double i = 0, remain_pay;
+long double calculate_interest(long double loan_amount, long double term, long double apr){
+  long double interest, i = 0;
   term = term * 12;
   while (i < term){
-    i = i + 1;
-    remain_pay = mon_pay * (i);
+  i = i + 1;
+  interest = loan_amount * (apr / 12);
   }
-  printf("remain pay = %Lf\n", remain_pay);
-  return remain_pay;
-  }
-
-  long double calculate_interest(long double loan_amount, long double term, long double apr, long double mon_pay){
-    long double interest, i = 0;
-    term = term * 12;
-    while (i < term){
-    i = i + 1;
-    interest = loan_amount * (apr / 12);
-    loan_amount = mon_pay - interest;
-    printf("interest = %Lf, monthly_pay = %Lf\n", interest, mon_pay);
-  }
-    return interest;
+  return interest;
   }
 
-/*long double calculate_loan(long double loan_amount, long double mon_pay, long double interest, long double term){
+long double calculate_loan(long double loan_amount, long double term, long double apr){
   int i = 0;
-  long double principle, term;
+  long double principle, interest, total_interest = 0;
+  long double mon_pay = monthly_pay(loan_amount, term, apr);
+  printf("Month\t\tPrinciple\t\tInterest\t\tRemaining\n");
+  printf("-----------------------------------------------------------------------------\n");
+  term = term * 12;
   while (i < term){
   i = i + 1;
-  calculate_interest(loan_amount, )
+  interest = calculate_interest(loan_amount, term, apr);
+  total_interest = total_interest + interest;
   principle = mon_pay - interest;
-  printf("principle = %Lf\n", principle);
-  remaining_pay = loan_amount - principle;
-  printf("remaining = %Lf\n", principle);
-
+  loan_amount = loan_amount - principle;
+  printf("%d\t\t%Lf\t\t%Lf\t\t%Lf\n",i, principle, interest, loan_amount);
 }
- return remain_pay;
-}*/
+  printf("-----------------------------------------------------------------------------\n");
+  printf("Monthly payment = %Lf; total_interest = %Lf\n", mon_pay, total_interest);
+ return loan_amount;
+}
 
 
 int main() {
-  long double term, interest, mp;
-  interest = calculate_interest(10000, 2, 0.03, mp);
-  mp = monthly_pay(10000, 2, 0.03);
-  //calculate_loan(10000, mp, interest, 24);
-
-
+  long double term, apr, loan_amount;
+  printf("Loan amount: ");
+  scanf("%Lf", &loan_amount);
+  printf("Term in years (must be an integer): ");
+  scanf("%Lf", &term);
+  printf("Interest (for 5 percent, enter 0.05, etc.): ");
+  scanf("%Lf", &apr);
+  calculate_loan(loan_amount, term, apr);
   return 0;
 }
